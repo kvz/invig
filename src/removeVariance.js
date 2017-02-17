7cb63b3c95be121f870fed18e93e0d7c47a8bd19
Kevin van Zonneveld <kevin@vanzonneveld.net>
2017-02-17 13:26:33 +0100

    Add There appears to be trouble with your network connection to removeVariance

--- src/removeVariance.js
+++ src/removeVariance.js
@@ -32,6 +32,7 @@ const removeVariance = (input) => {
   input = input.replace(/^.*Fetching package.*\n/g, '')
   input = input.replace(/yarn install v\d+\.\d+\.\d+/g, 'yarn install vX.X.X')
   input = input.replace(/Done in \d+\.\d+s/g, 'Done in X.Xs')
+  input = input.replace(/^.*There appears to be trouble with your network connection.*\n/gm, '')
   input = input.replace(/^.*peer dependency "es6-promise.*\n/gm, '')
   input = input.replace(/^.*fsevents.*Excluding it from installation.*\n/gm, '')
   input = input.replace(/^.*fsevents.*incompatible with this module.*\n/gm, '')
