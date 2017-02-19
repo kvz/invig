8a2c3d4f94cfa97b2cd0b40d6366bdc9af373c59
Kevin van Zonneveld <kevin@vanzonneveld.net>
2017-02-19 11:08:08 +0100

    Remove more yarn variance

--- src/removeVariance.js
+++ src/removeVariance.js
@@ -29,13 +29,17 @@ const removeVariance = (input) => {
 
   input = stripAnsi(input)
 
-  input = input.replace(/^.*Fetching package.*\n/g, '')
-  input = input.replace(/yarn install v\d+\.\d+\.\d+/g, 'yarn install vX.X.X')
-  input = input.replace(/Done in \d+\.\d+s/g, 'Done in X.Xs')
-  input = input.replace(/^.*There appears to be trouble with your network connection.*\n/gm, '')
-  input = input.replace(/^.*peer dependency "es6-promise.*\n/gm, '')
+  input = input.replace(/^.*Already up-to-date.*\n/gm, '')
+  input = input.replace(/^.*Building fresh packages.*\n/gm, '')
+  input = input.replace(/^.*Fetching package.*\n/gm, '')
   input = input.replace(/^.*fsevents.*Excluding it from installation.*\n/gm, '')
   input = input.replace(/^.*fsevents.*incompatible with this module.*\n/gm, '')
+  input = input.replace(/^.*Linking dependencies.*\n/gm, '')
+  input = input.replace(/^.*peer dependency "es6-promise.*\n/gm, '')
+  input = input.replace(/^.*Resolving packages.*\n/gm, '')
+  input = input.replace(/^.*There appears to be trouble with your network connection.*\n/gm, '')
+  input = input.replace(/Done in \d+\.\d+s/g, 'Done in X.Xs')
+  input = input.replace(/yarn install v\d+\.\d+\.\d+/g, 'yarn install vX.X.X')
 
   // @todo: Remove this hack when scrolex no longer adds trailing spinner frames:
   cliSpinner.frames.forEach((frame) => {
